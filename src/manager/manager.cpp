d9017c569245f54a0b888dda87e77170fb73afda
paulo362 <p.eduardocm3@gmail.com>
2026-03-28 11:42:06 -0300

    Update manager.cpp

--- src/manager/manager.cpp
+++ src/manager/manager.cpp
@@ -4,53 +4,64 @@ using namespace std;
 
 Buffer_Manager::Buffer_Manager(){
     cache_hit = 0, cache_miss = 0;
-    memory = vector <registro> (5);
+    //memory = vector <registro> (5); nao quis apagar nada teu, mas acho que nao faz sentido -> ja tem no .h
+    memory.resize(5);
+    //inicializar a seed aleatoria (uma unica vez)
+    static bool seed = false;
+    if(!seed){
+        srand(static_cast<unsigned>(time(nullptr)));
+        seed = true;
+    }
 }
 
 string Buffer_Manager::fetch(int key){
-    for(registro& r : memory){
+    //verificar se a pagina ja ta no buffer
+    for(int i = 0 ; i < memory.size() ; i++){
+        registro r = memory[i];
         if(r.key == key){
             cache_hit++;
+            on_access(i, true); //notificar acesso (hit)
             return r.text;
         }
     }
 
     cache_miss++;
-    registro new_registro = acess_database(key);
+    registro new_registro = acess_database(key); // consultar no arquivo
 
-    bool isfull = true;
-    int pos;
-    for(int i = 0; i < 5; i++){
-        if(memory[i].key == 0){
-            isfull = false;
+    //procurar um slot vazio
+    int pos = -1;
+    for(int i = 0 ; i < memory.size() ; i++){
+        if(memory[i].key == 0) { // slot vazio
             pos = i;
+            break;
         }
     }
+    //se nao tiver slot vazio, aplica a politica de substituicao
+    if(pos == -1) pos = evict();
 
-    if(isfull) pos = evict();
     memory[pos] = new_registro;
+    on_access(pos, false); // notifica acesso (miss)
     return memory[pos].text;
 }
 
 void Buffer_Manager::displayCache(){
-    for(registro& r : memory){
-        if(r.key == 0) cout << "Registro vazio.\n";
-        else {
-            cout << "Chave -> " << r.key << ". ";  
-            cout << "Valor -> " << r.text << ". ";
-            cout << "Atualização -> " << r.update << ".\n";
-        }
+    cout << "Conteudo do Buffer:" << endl;
+    for (const auto& reg : memory) {
+        cout << "Chave: " << reg.key
+                << ", Valor: " << reg.text
+                << ", Atualizacao: " << (reg.update ? "true" : "false")
+                << endl;
     }
 }
 
 void Buffer_Manager::displayStats(){
-    cout << "Cache Hit: " << cache_hit << "\n";
-    cout << "Cache Miss: " << cache_miss << "\n";
+    cout << "Cache Hits: " << cache_hit << endl;
+    cout << "Cache Misses: " << cache_miss << endl;
 }
 
-// ToDo: ajeitar resto da função
 registro Buffer_Manager::acess_database(int key){
+    // ToDo: ajeitar resto da função
     fstream myfile;
-    myfile.open("../bancodedados.csv");
+    myfile.open("bancodedados.csv");
     // o que é a chave??
 }
